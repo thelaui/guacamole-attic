41c9438108e6c5f88408ead6396b9cd82c67ad96
Felix Lauer <felix.lauer@uni-weimar.de>
2011-12-15 13:37:55 +0100

    updated .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,12 @@
 
 void render(SceneGraph* graph, std::string const& display) {
     Renderer renderer;
-    renderer.add_display(800, 600, display);
-    renderer.start_render_loop(graph);
+    try {
+        renderer.add_display(800, 600, display);
+        renderer.start_render_loop(graph);
+    } catch (std::string& error) {
+        std::cerr<<error<<std::endl;
+    }
 }
 
 int main() {
@@ -67,24 +71,24 @@ int main() {
 //        }
 //    }
 //
-    for (int i(0); i < 10; ++i) {
-        std::stringstream papa;
-        papa << "papa" << i;
-        monkey = graph.add_node("/", papa.str(), monkey_core);
-        monkey.translate(0.3*i-2, 1, 0);
-        monkey.scale(0.1, 0.1, 0.1);
-
-        std::string path("/" + papa.str());
-        for (int j(0); j < 10; ++j) {
-            std::stringstream child;
-            child << "monk" << j;
-            monkey = graph.add_node(path, child.str(), monkey_core);
-            monkey.scale(0.9, 0.9, 0.9);
-            monkey.translate(0, j, 0);
-
-            path = path + "/" + child.str();
-        }
-    }
+//    for (int i(0); i < 10; ++i) {
+//        std::stringstream papa;
+//        papa << "papa" << i;
+//        monkey = graph.add_node("/", papa.str(), monkey_core);
+//        monkey.translate(0.3*i-2, 1, 0);
+//        monkey.scale(0.1, 0.1, 0.1);
+//
+//        std::string path("/" + papa.str());
+//        for (int j(0); j < 10; ++j) {
+//            std::stringstream child;
+//            child << "monk" << j;
+//            monkey = graph.add_node(path, child.str(), monkey_core);
+//            monkey.scale(0.9, 0.9, 0.9);
+//            monkey.translate(0, j, 0);
+//
+//            path = path + "/" + child.str();
+//        }
+//    }
 
 
     //std::cout << graph << std::endl;
