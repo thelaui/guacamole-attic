cb886777e324ad514edd6480c2ed915800e04969
thelaui <felix.lauer@uni-weimar.de>
2012-04-18 13:25:33 +0200

    tweaked tracking example

--- tracking_example/main.cpp
+++ tracking_example/main.cpp
@@ -24,13 +24,13 @@
 #include <scm/input/tracking/art_dtrack.h>
 #include <scm/input/tracking/target.h>
 
-gua::RenderPipeline* create_pipe() {
+gua::RenderPipeline* create_pipe(std::string const& camera, std::string const& screen, std::string const& display) {
     // setup rendering pipeline
-    auto pass = new gua::RenderPass("main", "camera", "screen");
+    auto pass = new gua::RenderPass("main", camera, screen);
     pass->add_buffer(gua::ColorBufferDescription("color", 0));
     pass->add_buffer(gua::DepthStencilBufferDescription("depth_stencil"));
 
-    auto pipe = new gua::RenderPipeline(gua::RenderWindow::Description(1600, 900, "tracking_example", ":0.0", gua::ANAGLYPH_RED_CYAN));
+    auto pipe = new gua::RenderPipeline(gua::RenderWindow::Description(1600, 600, "tracking_example", display, gua::SIDE_BY_SIDE));
     pipe->add_render_pass(pass);
     pipe->set_final_buffer("main", "color");
 
@@ -53,6 +53,7 @@ int main(int argc, char** argv) {
 
     scm::inp::tracker::target_container targets;
     targets.insert(scm::inp::tracker::target_container::value_type(1, scm::inp::target(1)));
+    targets.insert(scm::inp::tracker::target_container::value_type(2, scm::inp::target(2)));
 
     // setup scene
     gua::SceneGraph graph;
@@ -67,20 +68,20 @@ int main(int argc, char** argv) {
     box.scale(500, 500, 500);
     box.translate(0, 250, -2150);
 
-//    auto monkey_core = new gua::GeometryCore("monkey", "wood");
-//    auto ape = graph.add_node("/box", "monkey", monkey_core);
-//    ape.scale(0.5, 0.5, 0.5);
-//    ape.translate(0, 1, 0);
+    auto monkey_core = new gua::GeometryCore("monkey", "wood");
+    auto ape = graph.add_node("/box", "monkey", monkey_core);
+    ape.scale(0.5, 0.5, 0.5);
+    ape.translate(0, 1.5, 0);
 
     auto screen_core(new gua::ScreenCore(4000, 3000));
     auto screen = graph.add_node("/", "screen", screen_core);
     screen.translate(0, 1500, -1900);
 
     auto camera_core = new gua::CameraCore(70.f);
-    auto camera = graph.add_node("/", "camera", camera_core);
-   // camera.translate(0, 0, 1.5);
+    graph.add_node("/", "camera1", camera_core);
+    graph.add_node("/", "camera2", camera_core);
 
-    gua::RenderServer renderer({create_pipe()});
+    gua::RenderServer renderer({create_pipe("camera1", "screen", ":0.0"), create_pipe("camera2", "screen", ":0.0")});
 
     // application loop
     while (true) {
@@ -89,16 +90,21 @@ int main(int argc, char** argv) {
 	    // head target
 	    auto target_it = targets.find(1);
 
-	    if (target_it != targets.end()) {
-            graph["/camera"].set_transform(target_it->second.transform());
-            std::cout << target_it->second.transform() << std::endl;
-	    }
+	    if (target_it != targets.end())
+            graph["/camera1"].set_transform(target_it->second.transform());
+
+
+        target_it = targets.find(2);
+
+	    if (target_it != targets.end())
+            graph["/camera2"].set_transform(target_it->second.transform());
+
 
         renderer.queue_draw(&graph);
 
         //std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-//        graph["/box/monkey"].rotate(2, 0, 1, 0);
+        graph["/box/monkey"].rotate(2, 0, 1, 0);
 //        graph["/screen"].rotate(0.1, 0, 1, 0);
     }
 
